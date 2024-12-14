5a29caa8535fd042f0189113f763c772d15f178a
xduzdb <1679046112@qq.com>
2024-12-14 23:03:33 +0800

    feat:增加暗色模式

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:weatherdashboard/ui//screens/dashboard_screen.dart'; // 导入 DashboardScreen
+import 'package:weatherdashboard/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:weatherdashboard/Controller/menu_app_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +17,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // 去掉调试标志
       title: 'Weather Dashboard',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
       ),
-      home: const DashboardScreen(), // 设置主页面为 DashboardScreen
+      // home: const DashboardScreen(), // 设置主页面为 DashboardScreen
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuAppController(),
+          ),
+        ],
+        child: DashboardScreen(),
+      ),
     );
   }
 }
